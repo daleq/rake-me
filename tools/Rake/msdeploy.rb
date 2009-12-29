fea15d2b93b85652e657443affc7a8facd9eb6c8
Alexander Gross <agross@therightstuff.de>
2009-12-29 16:38:04 +0100

    Supporting multiple MSDeploy skip directives

--- tools/Rake/msdeploy.rb
+++ tools/Rake/msdeploy.rb
@@ -1,15 +1,43 @@
 class MSDeploy
 	def self.run(attributes)
 		tool = attributes.fetch(:tool)
+		tee_tool = attributes.fetch(:tee_tool, "tee.exe".in(tool.dirname))
+		logFile = attributes.fetch(:log_file, "msdeploy.log")
 		
 		attributes.reject! do |key, value|
-			key == :tool
+			key == :tool || key == :log_file
 		end
 		
+		switches = generate_switches(attributes)
+		
+		msdeploy = tool.to_absolute
+		tee = tee_tool.to_absolute
+		
+		sh "#{msdeploy.escape} #{switches} 2>&1 | #{tee.escape} -a #{logFile}" do
+			doc = File.read(logFile)
+			errors = doc.scan(/error|exception|fehler/i)
+			
+			if errors.any?
+				raise "\nDeployment errors occurred. Please review #{logFile}."
+			else
+				puts "\nDeployment successful."
+			end
+		end
+	end
+	
+	def self.generate_switches(attributes)
 		switches = ""
 		
-		attributes.each do |key, value|
-			switches += "-#{key}#{":#{value}" unless value.kind_of? Enumerable or value.kind_of? TrueClass or value.kind_of? FalseClass}" if value
+		attributes.each do |switch, value|
+			if value.kind_of? Array
+				switches += value.collect { |element|
+					generate_switches ({ switch => element })
+				}.join
+				
+				next
+			end
+			
+			switches += "-#{switch}#{":#{value}" unless value.kind_of? Enumerable or value.kind_of? TrueClass or value.kind_of? FalseClass}" if value		
 			
 			if value.kind_of? Enumerable
 				switches += ":"
@@ -21,8 +49,6 @@ class MSDeploy
 			switches += " "
 		end
 		
-		msdeploy = tool.to_absolute
-		
-		sh "#{msdeploy.escape} #{switches}"
+		switches
 	end
 end
